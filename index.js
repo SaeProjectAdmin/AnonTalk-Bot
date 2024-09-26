d81d2346c329c5c055160d421fc139067db2425e
Arik Riko Prasetya <49115520+rikoarik@users.noreply.github.com>
2024-09-26 20:43:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
 const { Telegraf } = require('telegraf');
 const userCheck = require('./middleware/userCheck');
-const db = require('./db'),
-    cfg = require('./config'),
-    express = require('express'),
-    bodyParser = require("body-parser"),
-    commands = require('./command/commands'),
-    userSession = require('./session/sessions')
+const db = require('./db');
+const cfg = require('./config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const commands = require('./command/commands');
+const userSession = require('./session/sessions');
 
-const token = cfg.BOT_TOKEN
-const bot = new Telegraf(token)
+const token = cfg.BOT_TOKEN;
+const bot = new Telegraf(token);
 const app = express();
 
-const port = 5354
-const secretPath = '/' + token
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+const port = process.env.PORT || 4000;
+const secretPath = '/' + token;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get("/", function (request, resolve) {
-    resolve.end('Service is running.')
+app.get('/', (req, res) => {
+    res.status(200).json('Welcome, your app is working well');
 });
 
+app.listen(port, () => console.log(`Server ready on port ${port}.`));
+
 db.init(() => {
+    // Bot middleware
     bot.use(async (ctx, next) => {
-        userCheck(ctx, next)
-    })
-    bot.start((ctx) => commands.start(ctx))
-    bot.command('avatar', (ctx) => commands.settings.setAva(ctx))
-    bot.command('lang', (ctx) => commands.settings.setLang(ctx))
-    bot.command('cancel', (ctx) => commands.cancel(ctx))
-    bot.command('join', (ctx)=>commands.join(ctx))
-    bot.command('exit', (ctx)=>commands.exit(ctx))
-    bot.command('rooms', (ctx)=>commands.rooms(ctx))
-    bot.command('list', (ctx)=>commands.list(ctx))
-    bot.command('donate', (ctx)=>commands.donate(ctx))
-    bot.command('help', (ctx)=>commands.help(ctx))
-    bot.on('message', (ctx) => userSession(ctx))
-    bot.launch()
-    app.use(bot.webhookCallback(secretPath))
-    app.listen(process.env.PORT || port, () => {
-        console.log(`Refferal app listening on port ${process.env.PORT || port}!`)
-    })
-})
+        await userCheck(ctx, next);
+    });
+
+    // Bot commands
+    bot.start((ctx) => commands.start(ctx));
+    bot.command('avatar', (ctx) => commands.settings.setAva(ctx));
+    bot.command('lang', (ctx) => commands.settings.setLang(ctx));
+    bot.command('cancel', (ctx) => commands.cancel(ctx));
+    bot.command('join', (ctx) => commands.join(ctx));
+    bot.command('exit', (ctx) => commands.exit(ctx));
+    bot.command('rooms', (ctx) => commands.rooms(ctx));
+    bot.command('list', (ctx) => commands.list(ctx));
+    bot.command('donate', (ctx) => commands.donate(ctx));
+    bot.command('help', (ctx) => commands.help(ctx));
+    bot.on('message', (ctx) => userSession(ctx));
+
+    // Bot webhook setup
+    bot.launch();
+    app.use(bot.webhookCallback(secretPath));
+    
+    // Log when app is ready
+    console.log(`Referral app listening on port ${port}!`);
+});
+
+module.exports = app;
