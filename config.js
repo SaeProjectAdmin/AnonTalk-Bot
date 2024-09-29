5903cdc9af3a4eae113940ba1214e082738f2510
rikoarik <rikoarik04@gmail.com>
2024-09-29 17:45:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,9 @@
-require('dotenv').config();  // Memuat environment variables dari file .env
+require('dotenv').config();
 const fs = require('fs');
 
-// Jika menggunakan environment variable untuk path service account, atau fallback ke path default
-const firebaseCredentialsPath = process.env.FIREBASE_CREDENTIALS || "./path/to/default/serviceAccount.json";
-let firebaseCredentials;
-
-try {
-    // Coba memuat file service account jika path tersedia
-    firebaseCredentials = JSON.parse(fs.readFileSync(firebaseCredentialsPath, 'utf8'));
-} catch (err) {
-    console.error("Failed to load Firebase credentials:", err);
-    process.exit(1);  // Keluar jika gagal memuat kredensial Firebase
-}
-
 module.exports = {
-    FIREBASE_CREDENTIALS: firebaseCredentials,
-    DB_URL: process.env.DB_URL || "",  // Pastikan DB_URL ada di .env
-    BOT_TOKEN: process.env.BOT_TOKEN || "",  // Pastikan BOT_TOKEN ada di .env
-    BOT_NAME: process.env.BOT_NAME || "default_bot_name"  // BOT_NAME bisa memiliki default
+    FIREBASE_CREDENTIALS: JSON.parse(fs.readFileSync(process.env.FIREBASE_CREDENTIALS || './serviceAccount.json', 'utf8')), // Memuat serviceAccount.json sebagai object
+    DB_URL: process.env.DB_URL || "",
+    BOT_TOKEN: process.env.BOT_TOKEN || "",
+    BOT_NAME: process.env.BOT_NAME || "default_bot_name"
 };
